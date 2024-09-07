98d2c0ab3ae6b9316cbc1878fce4649ec1d164b4
Theephiga-Balendran <Theephiga.Balendran@city.ac.uk>
2024-09-07 10:03:50 +0100

    RewardValue

---
+++ src/main/java/RewardValue.java
@@ -0,0 +1,30 @@
+public class RewardValue {
+
+    private static final double MILES_TO_CASH_RATE = 0.0035;
+
+    private double cashValue;
+
+    private double milesValue;
+
+    public RewardValue(double cashValue){
+        this.cashValue = cashValue;
+        this.milesValue = cashValue/ MILES_TO_CASH_RATE;
+    }
+
+    public RewardValue(double milesValue, boolean isMiles){
+        this.milesValue = milesValue;
+        this.cashValue = milesValue * MILES_TO_CASH_RATE;
+
+    }
+
+
+    public double getMilesValue() {
+        return milesValue;
+    }
+
+    public double getCashValue() {
+        return cashValue;
+    }
+
+
+}
